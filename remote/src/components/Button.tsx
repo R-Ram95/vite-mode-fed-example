b7b349463b0e3efcdb5dbda1be1dffe60517a3ee
R-Ram95 <rohinesh@ualberta.ca>
2024-02-20 10:00:41 -0700

    init

---
+++ remote/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Button = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(() => count + 1)}>
+        Click me: {count}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
